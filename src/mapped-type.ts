3c00f178d34c45448ff8b809ab133f741deb972c
rubelrana123 <rubelranatpi707458@gmail.com>
2023-05-06 01:11:35 +0600

    added mapped code

--- src/mapped-type.ts
+++ src/mapped-type.ts
@@ -1,3 +1,36 @@
 const arrayofnumber = [1,2,3];
 const arraystring  = arrayofnumber.map((number) => number.toString);
 console.log(arraystring);
+//or
+type AreaNumber = {
+    height : number;
+    width : string;
+    dept : number
+}
+type Area <T> = {
+    // [key in keyof AreaNumber] : number;
+    // [key in keyof AreaNumber] : AreaNumber[key];
+   readonly [key in keyof T] : T[key]
+
+}
+const Area2 : Area <{name : string}> = {name : "Rubel"};
+
+type AreaString  ={
+    height : string;
+    width : string
+}
+type AreaReadOnly = {
+    height : number;
+    width : number
+}
+const  retangularArea : AreaNumber = {
+    height : 263,
+    width : 54
+}
+
+type A1 = Area["height"];
+type B2 = keyof Area;
+// const  obj = {
+//     name : "Persian",
+// }
+// obj.name;
